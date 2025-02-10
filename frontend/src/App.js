3f618e59cbef1d08a866e5f0f49e271680f88338
Junwon <junwon1022@naver.com>
2025-02-10 14:35:09 +0100

    Import Login.js in App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "./components/Login";
+
+function App() {
+    return (
+        <div>
+            <h1>Fullstack Login</h1>
+            <Login />
+        </div>
+    );
+}
+
+export default App;
